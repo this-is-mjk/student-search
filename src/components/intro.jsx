c0086e37c09a28593bae1e7e4582bae53bb50754
AkshatIITk <iitkakshat@gmail.com>
2024-07-27 20:57:42 +0530

    added gender filter and enhanced the ui

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -7,20 +7,22 @@ export default function Intro() {
       <TypeAnimation
         sequence={[
           // Same substring at the start will only be typed out once, initially
-          "Want to know your batchmates?",
-          1000, // wait 2s before replacing "Mice" with "Hamsters"
-          "Want to Reach out to them?",
+          "Looking to connect with your batchmates?",
+          1000, // wait 1s before replacing the text
+          "Need to get in touch with them?",
           1000,
-          "Want to search your juniors?",
+          "Introducing Student Search Y24 Exclusive: Your Gateway to Connection",
           1000,
         ]}
         wrapper="div"
         speed={50}
         style={{
+          fontFamily:"serif",
+          fontWeight: 200,
           fontSize: "2em",
           alignItems: "start",
           textWrap: "nowrap",
-          textAlign: "center"
+          textAlign: "center",
         }}
         repeat={Infinity}
       />
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -15,34 +15,37 @@ export default function SearchBar({ searchTerm, setSearchTerm }) {
   const [selectedBranch, setSelectedBranch] = useState(searchTerm.department);
 
   const toggleSelection = (item, type) => {
-    const setter = type === 'gender' ? setSelectedGender : setSelectedBranch;
-    const selected = type === 'gender' ? selectedGender : selectedBranch;
-
     let updatedSelection;
-    if (selected.includes(item)) {
-      updatedSelection = selected.filter(i => i !== item);
+    
+    if (type === 'gender') {
+      updatedSelection = selectedGender.includes(item) ? [] : [item];
+      setSelectedGender(updatedSelection);
     } else {
-      updatedSelection = [...selected, item];
+      updatedSelection = selectedBranch.includes(item)
+        ? selectedBranch.filter(i => i !== item)
+        : [...selectedBranch, item];
+      setSelectedBranch(updatedSelection);
     }
-    setter(updatedSelection);
+
     setSearchTerm(searchTerm => ({ ...searchTerm, [type]: updatedSelection }));
   };
+
   const handleInputChange = (e) => {
     setSearchTerm({ ...searchTerm, text: e.target.value });
   };
 
   return (
     <>
-      <label>
-      <Popover>
-        <PopoverAnchor className="outer-searchbar-box">
-          <FaSearch style={{ color: "white" }} />
-          <input
-            className="searchbar"
-            type="text"
-            placeholder="Search"
-            onChange={handleInputChange}
-          />
+      <label className="">
+        <Popover>
+          <PopoverAnchor className="outer-searchbar-box">
+            <FaSearch style={{ color: "white" }} />
+            <input
+              className="searchbar lg:w-[40vw]"
+              type="text"
+              placeholder="Search"
+              onChange={handleInputChange}
+            />
             <PopoverTrigger>
               <IconButton
                 className="filter-button"
@@ -52,37 +55,35 @@ export default function SearchBar({ searchTerm, setSearchTerm }) {
             </PopoverTrigger>
             <PopoverContent className="w-[40vw] min-w-[300px]">
               <div className="overflow">
-                <p>Gender</p>
+                <p className="font-bold font-serif mb-4 text-center lg:text-2xl text-xl">Gender</p>
                 <div className="buttonContainer">
-                  {['Male', 'Female', 'Other'].map(gender => (
+                  {['Male', 'Female'].map(gender => (
                     <button
                       key={gender}
-                      className={selectedGender.includes(gender) ? 'selected menu-button' : 'not-selected menu-button'}
+                      className={selectedGender.includes(gender) ? 'selected menu-button text-3xl' : 'not-selected menu-button text-xl'}
                       onClick={() => toggleSelection(gender, 'gender')}
                     >
                       {gender}
                     </button>
                   ))}
-                  </div>
-                  <p>Branch</p>
-                  <div className="buttonContainer branch">
+                </div>
+                <p className="font-bold font-serif my-4 text-center lg:text-2xl text-xl">Branch</p>
+                <div className="buttonContainer branch ">
                   {['Chemistry', 'Electrical Engineering', 'Materials Science and Engineering', 'Chemical Engineering', 'Mechanical Engineering', 'Economics', 'Civil Engineering', 'Physics', 'Computer Science and Engineering', 'Mathematics and Statistics', 'Biological Sciences and Bioengineering', 'Aerospace Engineering', 'Earth Science', 'Statistics and Data Science'].map(branch => (
                     <button
                       key={branch}
-                      className={selectedBranch.includes(branch) ? 'selected menu-button branch-button' : 'not-selected menu-button branch-button'}
+                      className={selectedBranch.includes(branch) ? 'selected menu-button branch-button lg:text-md text-sm' : 'not-selected menu-button branch-button lg:text-md text-sm'}
                       onClick={() => toggleSelection(branch, 'department')}
                     >
                       {branch}
                     </button>
-              ))}
-
-                  </div>
-                  
-            </div>
+                  ))}
+                </div>
+              </div>
             </PopoverContent>
-        </PopoverAnchor>
+          </PopoverAnchor>
         </Popover>
-        </label>
+      </label>
     </>
   );
 }
